a98b59d3043143fb47be594e3d65357eaf4e82a8
jchanghong <3200601@qq.com>
2017-06-15 19:06:59 +0800

    增加登陆和sql的日志记录到elasticsearch

---
+++ src/main/java/io/jsql/estest.kt
@@ -0,0 +1,14 @@
+package io.jsql
+
+import org.elasticsearch.bootstrap.Elasticsearch
+
+/**
+\* Created with IntelliJ IDEA.
+\* User: jiang
+\* Date: 2017/6/15 0015
+\* Time: 17:51
+\*/
+fun main(args: Array<String>) {
+    Elasticsearch.main(args)
+
+}
