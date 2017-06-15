ea50f189bda009a3baa3a64b93fb12ba677ad1ba
jchanghong <3200601@qq.com>
2017-06-15 20:00:46 +0800

    启动elastic服务器

--- src/main/java/io/jsql/SpringMain.kt
+++ src/main/java/io/jsql/SpringMain.kt
@@ -4,6 +4,7 @@ import com.hazelcast.map.impl.client.MapPortableHook
 import io.jsql.hazelcast.MyHazelcast
 import io.jsql.netty.NettyServer
 import io.jsql.sql.OConnection
+import org.elasticsearch.bootstrap.Elasticsearch
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
@@ -11,6 +12,8 @@ import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.core.annotation.Order
+import java.util.concurrent.CountDownLatch
+import kotlin.concurrent.thread
 
 /**
  * Created by 长宏 on 2017/5/2 0002.
@@ -29,6 +32,7 @@ open class SpringMain : CommandLineRunner {
     override fun run(vararg strings: String) {
         Runtime.getRuntime().addShutdownHook(hook)
         logger.info("begin start....................................")
+        thread (start = true){  Elasticsearch.main(strings)}
         try {
             nettyServer.start()
         } catch (e: Exception) {
