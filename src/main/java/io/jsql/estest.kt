c505defdc60fa97692471b995109f56464f3cecf
jchanghong <3200601@qq.com>
2017-06-15 19:52:11 +0800

    启动elastic服务器

--- src/main/java/io/jsql/estest.kt
+++ src/main/java/io/jsql/estest.kt
@@ -1,14 +1,20 @@
 package io.jsql
 
 import org.elasticsearch.bootstrap.Elasticsearch
+import java.util.concurrent.CountDownLatch
+import kotlin.concurrent.thread
 
 /**
 \* Created with IntelliJ IDEA.
 \* User: jiang
 \* Date: 2017/6/15 0015
 \* Time: 17:51
+启动elasticsearch服务器
 \*/
 fun main(args: Array<String>) {
-    Elasticsearch.main(args)
+    var down = CountDownLatch(1)
+    thread (start = true){  Elasticsearch.main(args)}
+    println("start.......")
+    down.await()
 
 }
