78ec3dde68f2035d0a1ec397493688964d1ca028
jchanghong <3200601@qq.com>
2017-08-25 09:19:48 +0800

    spring aop

---
+++ src/main/java/io/jsql/springaop/LogAop.java
@@ -0,0 +1,37 @@
+/*
+ * Java-based distributed database like Mysql
+ */
+
+package io.jsql.springaop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * \* Created with IntelliJ IDEA.
+ * \* User: jiang
+ * \* Date: 2017/8/25 0025
+ * \* Time: 9:04
+ * \
+ */
+@Aspect
+@Component
+public class LogAop {
+    private final boolean debug = true;
+    private static Logger logger = LoggerFactory.getLogger(LogAop.class);
+    @Pointcut("within(io.jsql..*)")
+    public void point1() {
+    }
+
+//    @Before("point1()")
+    public void log(JoinPoint joinPoint) {
+        if (debug) {
+            logger.info(joinPoint.getTarget().getClass().toGenericString()+joinPoint.toShortString());
+        }
+    }
+}
