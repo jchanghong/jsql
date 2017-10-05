22e1cdce1f21050b849ebc91fca6f108305181a6
jchanghong <3200601@qq.com>
2017-10-05 13:22:56 +0800

    测试

--- src/main/java/io/jsql/test/JDBC.kt
+++ src/main/java/io/jsql/test/JDBC.kt
@@ -5,7 +5,6 @@
 package io.jsql.test
 
 import java.sql.DriverManager
-import java.sql.SQLException
 
 /**
  * Created by 长宏 on 2017/4/30 0030.
@@ -14,14 +13,54 @@ import java.sql.SQLException
 object JDBC {
     @JvmStatic
     fun main(args: Array<String>) {
-        val url = "jdbc:mysql://localhost:9999/changhong?" + "user=root&password=0000&useUnicode=true&characterEncoding=UTF8"
-        try {
-            val connection = DriverManager.getConnection(url)
-            println(connection.autoCommit)
-            println(connection.catalog)
-        } catch (e: SQLException) {
-            e.printStackTrace()
+//        val url = "jdbc:mysql://localhost:3306/changhong?" + "user=root&password=0000&useUnicode=" +
+//                "true&characterEncoding=UTF8"
+//        val url2 = "jdbc:mysql://localhost:9999/changhong?" + "user=root&password=0000&useUnicode=" +
+//                "true&characterEncoding=UTF8"
+//        try {
+//            val connection = DriverManager.getConnection(url2)
+//            val statement = connection.createStatement()
+//            println(connection.autoCommit)
+//            val result = statement.executeQuery("select * from test")
+//            while (result.next()) {
+//                println(result.getString(1))
+//            }
+//        } catch (e: SQLException) {
+//            e.printStackTrace()
+//        }
+        JDBC.test()
+    }
+
+    fun test() {
+        val sqlnumber = intArrayOf(10, 100, 1000, 3000, 5000, 10000) //sql执行次数
+        val mysql = "jdbc:mysql://localhost:3306/changhong?" + "user=root&" +
+                "password=0000&useUnicode=" + "true&characterEncoding=UTF8"
+        var connection = DriverManager.getConnection(mysql)
+        var statement = connection.createStatement()
+        println("mysql sqlnumber to times:")
+        for (number in sqlnumber) {
+            val start = System.currentTimeMillis()
+            for (i in 1..number) {
+                statement.executeQuery("select * from test")
+            }
+            val end = System.currentTimeMillis()
+            println("$number      ${end - start}")
         }
+        connection.close()
+        val jsql = "jdbc:mysql://localhost:9999/changhong?" + "user=root&" +
+                "password=0000&useUnicode=" + "true&characterEncoding=UTF8"
+        connection = DriverManager.getConnection(jsql)
+        statement = connection.createStatement()
+        println("jsql sqlnumber to times:")
+        for (number in sqlnumber) {
+            val start = System.currentTimeMillis()
+            for (i in 1..number) {
+                statement.executeQuery("select * from test")
+            }
+            val end = System.currentTimeMillis()
+            println("$number      ${end - start}")
+        }
+        connection.close()
 
     }
 }
